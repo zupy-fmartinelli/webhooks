bb55249475cc0f2ce60efe7d3f2b7e57a3963b3a
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-30 21:36:26 -0300

    Configurando Rota do Webhook

--- flaskapp.py
+++ flaskapp.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Welcome to Flask Application!"
+
+@app.route("/webhoook")
+def pontuado():
+    return "Webhooks"    
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+    
