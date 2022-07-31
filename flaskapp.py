d4152e76f0f3d08a5ea4a817c8d024f9d49e7c95
Fabio Martinelli <webmaster@zupy.com.br>
2022-07-30 22:11:12 -0300

    Configurado Integracao Firebase

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,21 @@
+from flask import json
+from flask import request
 from flask import Flask
+
+import firebase_admin
+from firebase_admin import credentials
+
+# Importando o modulo do Firestore
+from firebase_admin import firestore
+
+# criando a api key e salvando
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+# criar uma variave para associar ao banco de dados do firestore
+db=firestore.client()
+
+
 app = Flask(__name__)
 @app.route('/')
 def hello():
@@ -6,7 +23,14 @@ def hello():
 
 @app.route('/webhooks')
 def zupyhooks():
-    return 'Hello, World'
+      if request.headers['Content-Type'] == 'application/json':
+        my_info = json.dumps(request.json)
+        print(my_info)        
+    #Salva o arquivo no Firebase no formato Json que retornou
+        db.collection('zupyhooks').add(request.json)
+    #retorna o token de autenticação
+        return my_info
+
 
 
 if __name__ == "__main__":
